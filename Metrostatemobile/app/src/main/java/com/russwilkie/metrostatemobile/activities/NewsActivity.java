5b531320ed45d43c35694371ef8b0afaf44bbb01
Russ Wilkie <russwilkie@gmail.com>
2017-11-06 16:12:32 -0600

    Updated JSON Parser Adapter

--- Metrostatemobile/app/src/main/java/com/russwilkie/metrostatemobile/activities/NewsActivity.java
+++ Metrostatemobile/app/src/main/java/com/russwilkie/metrostatemobile/activities/NewsActivity.java
@@ -1,14 +1,10 @@
 package com.russwilkie.metrostatemobile.activities;
 
-import android.net.http.RequestQueue;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
+
+import android.widget.ListView;
 
 import com.russwilkie.metrostatemobile.Config;
 import com.russwilkie.metrostatemobile.R;
@@ -17,28 +13,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-//import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.russwilkie.metrostatemobile.adapters.JSONListAdapter;
 
 import java.util.ArrayList;
 
-public class NewsActivity extends AppCompatActivity implements Spinner.OnItemSelectedListener{
+public class NewsActivity extends AppCompatActivity {
 
-    private Spinner spinner;
-
-    //An ArrayList for Spinner Items
-    private ArrayList<String> students;
-
-    //JSON Array
     private JSONArray result;
-
-    //TextViews to display details
-    private TextView textViewName;
-    private TextView textViewCourse;
-    private TextView textViewSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,23 +35,8 @@ public class NewsActivity extends AppCompatActivity implements Spinner.OnItemSel
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        students = new ArrayList<String>();
-
-        //Initializing Spinner
-        spinner = (Spinner) findViewById(R.id.spinner);
-
-        //Adding an Item Selected Listener to our Spinner
-        //As we have implemented the class Spinner.OnItemSelectedListener to this class iteself we are passing this to setOnItemSelectedListener
-        spinner.setOnItemSelectedListener(this);
-
-        textViewName = (TextView) findViewById(R.id.textViewName);
-        textViewCourse = (TextView) findViewById(R.id.textViewCourse);
-        textViewSession = (TextView) findViewById(R.id.textViewSession);
-
-        //This method will fetch the data from the URL
         getData();
-    } //Initializing TextViews
-
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -74,8 +44,8 @@ public class NewsActivity extends AppCompatActivity implements Spinner.OnItemSel
         return true;
     }
 
+    //Fetches data from URL
     private void getData(){
-        //Creating a string request
         StringRequest stringRequest = new StringRequest(Config.DATA_URL,
                 new Response.Listener<String>() {
                     @Override
@@ -85,11 +55,14 @@ public class NewsActivity extends AppCompatActivity implements Spinner.OnItemSel
                             //Parsing the fetched Json String to JSON Object
                             j = new JSONObject(response);
 
-                            //Storing the Array of JSON String to our JSON Array
                             result = j.getJSONArray(Config.JSON_ARRAY);
 
-                            //Calling method getStudents to get the students from the JSON Array
-                            getStudents(result);
+                            ArrayList<JSONObject> listItems=getArrayListFromJSONArray(result);
+
+                            ListView listV=(ListView)findViewById(R.id.listview);
+                            JSONListAdapter adapter= new JSONListAdapter(NewsActivity.this, R.layout.json_listview, R.id.txtheader,listItems);
+                            listV.setAdapter(adapter);
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -101,88 +74,23 @@ public class NewsActivity extends AppCompatActivity implements Spinner.OnItemSel
 
                     }
                 });
-
-        //Creating a request queue
         com.android.volley.RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-        //Adding request to the queue
         requestQueue.add(stringRequest);
     }
 
-    private void getStudents(JSONArray j){
-        //Traversing through all the items in the json array
-        for(int i=0;i<j.length();i++){
-            try {
-                //Getting json object
-                JSONObject json = j.getJSONObject(i);
+    private ArrayList<JSONObject> getArrayListFromJSONArray(JSONArray jsonArray){
+        ArrayList<JSONObject> aList=new ArrayList<JSONObject>();
 
-                //Adding the name of the student to array list
-                students.add(json.getString(Config.TAG_HEADER));
-            } catch (JSONException e) {
-                e.printStackTrace();
+        try {
+            if (jsonArray != null) {
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    aList.add(jsonArray.getJSONObject(i));
+                }
             }
-        }
+        }catch (JSONException je){je.printStackTrace();}
 
-        //Setting adapter to show the items in the spinner
-        spinner.setAdapter(new ArrayAdapter<String>(NewsActivity.this, android.R.layout.simple_spinner_dropdown_item, students));
-    }
-
-    //Method to get student name of a particular position
-    private String getName(int position){
-        String name="";
-        try {
-            //Getting object of given index
-            JSONObject json = result.getJSONObject(position);
-
-            //Fetching name from that object
-            name = json.getString(Config.TAG_LINK);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        //Returning the name
-        return name;
-    }
-
-    //Doing the same with this method as we did with getName()
-    private String getCourse(int position){
-        String course="";
-        try {
-            JSONObject json = result.getJSONObject(position);
-            course = json.getString(Config.TAG_DATE);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return course;
-    }
-
-    //Doing the same with this method as we did with getName()
-    private String getSession(int position){
-        String session="";
-        try {
-            JSONObject json = result.getJSONObject(position);
-            session = json.getString(Config.TAG_SUMMARY);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return session;
-    }
-
-
-    //this method will execute when we pic an item from the spinner
-    //@Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        //Setting the values to textviews for a selected item
-        textViewName.setText(getName(position));
-        textViewCourse.setText(getCourse(position));
-        textViewSession.setText(getSession(position));
-    }
-
-    //When no item is selected this method would execute
-    //@Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        textViewName.setText("");
-        textViewCourse.setText("");
-        textViewSession.setText("");
+        return  aList;
     }
 }
 
---
+++ Metrostatemobile/app/src/main/java/com/russwilkie/metrostatemobile/adapters/JSONListAdapter.java
@@ -0,0 +1,54 @@
+package com.russwilkie.metrostatemobile.adapters;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.russwilkie.metrostatemobile.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class JSONListAdapter extends ArrayAdapter<JSONObject> {
+
+    int vg;
+
+    ArrayList<JSONObject> list;
+
+    Context context;
+
+    public JSONListAdapter(Context context, int vg, int id, ArrayList<JSONObject> list) {
+        super(context, vg, id, list);
+
+        this.context = context;
+        this.vg = vg;
+        this.list = list;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(vg, parent, false);
+        TextView txtId = (TextView) itemView.findViewById(R.id.txtheader);
+        TextView txtName = (TextView) itemView.findViewById(R.id.txtsummary);
+        TextView txtSex = (TextView) itemView.findViewById(R.id.txtdate);
+
+        try {
+            txtId.setText(list.get(position).getString("news_header"));
+            txtName.setText(list.get(position).getString("news_summary"));
+            txtSex.setText(list.get(position).getString("news_date"));
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return itemView;
+    }
+
+}
