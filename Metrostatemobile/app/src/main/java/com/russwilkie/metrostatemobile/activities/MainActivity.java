b4441320b9fe131e8f5fa29e2d96ff05cad39777
izhar <gk1042mq@metrostate.edu>
2017-12-16 22:37:01 -0600

    added permissions

--- Metrostatemobile/app/src/main/java/com/russwilkie/metrostatemobile/activities/MainActivity.java
+++ Metrostatemobile/app/src/main/java/com/russwilkie/metrostatemobile/activities/MainActivity.java
@@ -1,7 +1,11 @@
 package com.russwilkie.metrostatemobile.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,10 +14,14 @@ import com.russwilkie.metrostatemobile.adapters.SwipeAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    final public static int REQUEST_CODE_PHONE_CALL = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestPermission();
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
         SwipeAdapter swipeAdapter = new SwipeAdapter(getSupportFragmentManager());
@@ -22,5 +30,11 @@ public class MainActivity extends AppCompatActivity {
         TabLayout pageIndicator = (TabLayout) findViewById(R.id.tab_layout);
         pageIndicator.setupWithViewPager(viewPager);
 
+    }
+
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CODE_PHONE_CALL);
+        }
     }
 }
